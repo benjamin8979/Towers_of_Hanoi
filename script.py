5e54e1ec212b098610f180fe6d10fe318045cd3a
codecademy <ccuser@codecademy.com>
2020-08-15 01:38:09 +0000

    Create the Stacks

---
+++ script.py
@@ -0,0 +1,17 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+#Set up the Game
+
+
+#Get User Input
+
+        
+#Play the Game
